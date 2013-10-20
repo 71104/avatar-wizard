a8140ffc9703635616c6437634a939502cc7c060
Alberto La Rocca <a71104@gmail.com>
2013-10-20 10:10:37 +0200

    Color picker

---
+++ js/ColorPicker.js
@@ -0,0 +1,47 @@
+function ColorPicker(huePicker, brightnessPicker) {
+	EventEmitter.call(this);
+	var thisObject = this;
+
+	this.register('pick');
+
+	var hue, saturation, brightness;
+
+	function getColor() {
+		return 'hsl(' + Math.round(hue * 360) + ', ' + Math.round(saturation * 100) + '%, ' + Math.round(brightness * 100) + '%)';
+	}
+
+	function setHue(x, y) {
+		saturation = x;
+		hue = y;
+		thisObject.emit('pick', getColor());
+	}
+
+	function setBrightness(x, y) {
+		brightness = 1 - y;
+		thisObject.emit('pick', getColor());
+	}
+
+	function handle(element, handler) {
+		element = $(element);
+		var width = element.innerWidth();
+		var height = element.innerHeight();
+		var dragging = false;
+		element.mousedown(function (event) {
+			dragging = true;
+			handler(event.clientX / width, event.clientY / height);
+			return false;
+		}).mousemove(function (event) {
+			if (dragging) {
+				handler(event.clientX / width, event.clientY / height);
+				return false;
+			}
+		}).mouseup(function () {
+			dragging = false;
+		});
+	}
+
+	handle(huePicker, setHue);
+	handle(brightnessPicker, setBrightness);
+
+	this.getColor = getColor;
+}
