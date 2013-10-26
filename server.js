c6484faa3ece5859350227d5a1e0999c64e83b2e
Alberto La Rocca <a71104@gmail.com>
2013-10-26 23:20:45 +0200

    Rescaffolding

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+app.use(express.static('static'));
+var server = http.createServer(app);
+server.listen(process.env.PORT || 80, function () {
+	require('openurl').open('http://localhost:' + server.address().port);
+});
