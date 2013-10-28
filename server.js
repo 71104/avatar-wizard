e101e3dcdc3720066c73d58888d04d2ecf95aa0e
Alberto La Rocca <a71104@gmail.com>
2013-10-28 11:18:46 +0100

    REST server

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ var http = require('http');
 var express = require('express');
 var app = express();
 app.use(express.static('static'));
+
+app.get('/avaparts', function (request, response) {
+	response.sendfile('data/avaparts.json');
+});
+
+app.get('/avaparts/:category', function (request, response) {
+	response.sendfile('data/categories/' + request.params.category + '.json');
+});
+
+app.get('/avaparts/:category/:id', function (request, response) {
+	response.sendfile('data/categories/' + request.params.category + '/' + request.params.id + '.js');
+});
+
 var server = http.createServer(app);
 server.listen(process.env.PORT || 80, function () {
 	require('openurl').open('http://localhost:' + server.address().port);
